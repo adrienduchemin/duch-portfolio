a11e833dc98058789dc59508713ff7ce355bf846
Adrien Duchemin <duchemin.adrien@gmail.com>
2021-07-23 14:45:20 +0200

    Add fresh ts project

--- utils/prismicPhotos.js
+++
@@ -1,6 +0,0 @@
-import Prismic from "prismic-javascript";
-
-export const API_URL = `https://duch-portfolio.cdn.prismic.io/api/v2`;
-export const API_TOKEN = process.env.NEXT_PUBLIC_PRISMIC_ACCESS_TOKEN;
-
-export const client = Prismic.client(API_URL, { accessToken: API_TOKEN });
